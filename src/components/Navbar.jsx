190d692207178ff1eb5873321fffaacce69950a5
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2022-01-14 14:22:14 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Navbar, Container, NavDropdown, Nav, Modal, Button, Form } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/nav.css'
+import { Badge } from "@material-ui/core";
+import {  ShoppingCartOutlined } from "@material-ui/icons";
+
+const navbar = () => {
+    return (
+        <div className="nav-bg" >
+         <container >
+         <Navbar  expand="lg">
+           <div className="logo" >
+         <img src="/logo.png"/>
+         </div>
+         <div className="about"> About </div>
+         <div class="dropdown">
+  <button class="dropbtn">Categories</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+<div className="contact">Contact </div>
+<div className="cart">
+<Badge badgeContent={0} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+        </div>
+       </Navbar>
+         </container>
+        </div>
+    )
+}
+
+export default navbar
